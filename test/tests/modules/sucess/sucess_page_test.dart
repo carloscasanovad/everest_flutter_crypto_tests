146933a837e3f512d61a4a056e93306ae044d413
carloscasanovad <carlos.casanova@warren.com.br>
2022-10-04 15:32:40 -0300

    feat/EV-870/SucessPage tests

---
+++ test/tests/modules/sucess/sucess_page_test.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:everest_flutter_crypto_tests/l10n/app_localizations_en.dart';
+import 'package:everest_flutter_crypto_tests/modules/sucess/sucess_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../helpers/setup_widget_tester.dart';
+
+main() {
+  Future<void> loadPage(
+    WidgetTester tester,
+  ) async {
+    const walletPageBody = SetupWidgetTester(
+      child: SuccessPage(),
+    );
+    await tester.pumpWidget(walletPageBody);
+  }
+
+  testWidgets(
+    "WHEN SucessPage is showed, THEN find a Hero widget with image and a success description",
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      final heroWidget = tester.widget<Hero>(find.byType(Hero));
+
+      expect(heroWidget.tag, 'check');
+      expect(find.byKey(const Key('successImage')), findsOneWidget);
+      expect(find.text(AppLocalizationsEn().successTitle), findsOneWidget);
+      expect(find.text(AppLocalizationsEn().successSubtitle), findsOneWidget);
+    },
+  );
+}
