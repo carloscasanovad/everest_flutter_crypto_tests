14a17766b5eec4a448c8d8b18fd79bd91283ec58
carloscasanovad <carlos.casanova@warren.com.br>
2022-10-04 09:40:26 -0300

    feat/EV-870/Shared Widgets: DefaultAppbar tests

--- test/shared_widgets_test.dart
+++ test/shared_widgets_test.dart
@@ -1,42 +1,52 @@
-import 'package:everest_flutter_crypto_tests/modules/sucess/sucess_page.dart';
-import 'package:everest_flutter_crypto_tests/modules/wallet/model/user_wallet_model.dart';
-import 'package:everest_flutter_crypto_tests/modules/wallet/widgets/header.dart';
 import 'package:everest_flutter_crypto_tests/shared/controllers/asset_provider.dart';
+import 'package:everest_flutter_crypto_tests/shared/widgets/bottom_nav_bar.dart';
 import 'package:everest_flutter_crypto_tests/shared/widgets/default_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'helpers/setup_widget_tester.dart';
 
+class Counter {
+  int value = 0;
+  void increment() => value++;
+  void decrement() => value--;
+}
+
 main() {
-  group("Wallet Page", () {
+  group("DefaultAppbar Test", () {
     Future<void> loadPage(WidgetTester tester,
         {required IconData icon,
         required VoidCallback function,
         required String title}) async {
-      var sucessPage = SetupWidgetTester(
-        child: DefaultAppBar(
-          icon: icon,
-          onPressed: function,
-          title: title,
-        ),
+      var defaultAppbar = SetupWidgetTester(
+        child: DefaultAppBar(icon: icon, onPressed: function, title: title),
       );
-      await tester.pumpWidget(sucessPage);
+      await tester.pumpWidget(defaultAppbar);
     }
 
-    testWidgets("SucessPage Test", (WidgetTester tester) async {
+    testWidgets(
+        "WHEN DefaultApp receives information, THEN ensure that the information is being used",
+        (WidgetTester tester) async {
+      final counter = Counter();
       const IconData specificIcon = Icons.abc;
       const String specificTitle = '';
+
       await loadPage(
         tester,
-        function: () {},
+        function: (() => counter.increment()),
         icon: specificIcon,
         title: specificTitle,
       );
+
       final defaultAppBar =
           tester.widget<DefaultAppBar>(find.byType(DefaultAppBar));
+
       expect(defaultAppBar.title, specificTitle);
       expect(defaultAppBar.icon, specificIcon);
+
+      await tester.tap(find.byType(IconButton));
+      expect(counter.value, 1);
     });
   });
+
 }
